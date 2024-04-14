a5ac140fb078d7c197f1a58f42997bac85a4e12a
unknown <sohanpk24@gmail.com>
2024-04-14 14:54:51 +0600

    firebase config secure

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -4,13 +4,17 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
+console.log('super secret ',import.meta.env.VITE_APIKEY);
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDFpGvGMZBRKir_mWh2G9m5yHT-ePW0aA0",
-  authDomain: "dragon-news-auth-ac151.firebaseapp.com",
-  projectId: "dragon-news-auth-ac151",
-  storageBucket: "dragon-news-auth-ac151.appspot.com",
-  messagingSenderId: "45091064474",
-  appId: "1:45091064474:web:134df86251e885b897ed23"
+  apiKey:import.meta.env.VITE_APIKEY,
+  authDomain:import.meta.env.VITE_AUTHDOMAIN,
+  projectId:import.meta.env.VITE_PROJECTID,
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+  appId:import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
--- src/routes/PrivetRoute.jsx
+++ src/routes/PrivetRoute.jsx
@@ -6,7 +6,7 @@ import { Navigate, useLocation } from "react-router-dom";
 const PrivetRoute = ({children}) => {
     const {user, loading}= useContext(AuthContext)
     const location = useLocation()
-    console.log(location);
+    console.log(location.pathname);
 
     if(loading){
         return <span className="loading loading-bars loading-lg"></span>
@@ -15,7 +15,7 @@ const PrivetRoute = ({children}) => {
     if(user){
         return children
     }
-    return <Navigate to='/login'></Navigate>
+    return <Navigate state={location.pathname} to='/login'></Navigate>
 };
 
 export default PrivetRoute;
